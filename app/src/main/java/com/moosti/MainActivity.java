1aec9cb8886270995a4d8e7693e5eaa44e55b1ac
dimkas71 <dimkas71g@gmail.com>
2015-06-12 20:26:38 +0300

    MO-9 Implement app Activity with toolbar instead of ActionBarActivity
    MO-7 Add icon "Tomato" to project and use it as App's main icon

--- app/src/main/java/com/moosti/MainActivity.java
+++ app/src/main/java/com/moosti/MainActivity.java
@@ -2,6 +2,7 @@ package com.moosti;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,12 +13,18 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setLogo(R.mipmap.ic_launcher);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
