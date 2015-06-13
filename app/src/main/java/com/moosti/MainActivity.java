73ad9794797e663bbb3b3f814e134f5532db432b
dimkas71 <dimkas71g@gmail.com>
2015-06-13 10:52:13 +0300

    MO-5 Try to add native android timer for time tracking....
    MO-10 Add handlers for .focus; .short brk; .long brk; .stop buttons

--- app/src/main/java/com/moosti/MainActivity.java
+++ app/src/main/java/com/moosti/MainActivity.java
@@ -1,13 +1,33 @@
 package com.moosti;
 
+import android.os.CountDownTimer;
+import android.os.SystemClock;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.format.DateUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private CountDownTimer timer;
+
+    private Button buttonFocus;
+    private Button buttonShortBreak;
+    private Button buttonLongBreak;
+    private Button buttonStop;
+    private TextView textView;
+    private StringBuilder recycled;
 
     enum CHRONOSTATE {
         FOCUSED,
@@ -27,7 +47,23 @@ public class MainActivity extends ActionBarActivity {
         setSupportActionBar(toolbar);
         toolbar.setLogo(R.mipmap.ic_launcher);
 
+        textView = (TextView)findViewById(R.id.textView);
+
         state = CHRONOSTATE.STOPPED;
+
+        buttonFocus = (Button)findViewById(R.id.btn_focus);
+        buttonFocus.setOnClickListener(this);
+
+        buttonShortBreak = (Button)findViewById(R.id.btn_short_break);
+        buttonShortBreak.setOnClickListener(this);
+
+        buttonLongBreak = (Button)findViewById(R.id.btn_long_break);
+        buttonLongBreak.setOnClickListener(this);
+
+        buttonStop = (Button)findViewById(R.id.btn_stop);
+        buttonStop.setOnClickListener(this);
+
+
 
     }
 
@@ -53,4 +89,99 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onClick(View v) {
+        final int id = v.getId();
+
+        switch (id) {
+            case R.id.btn_focus:
+
+                state = CHRONOSTATE.FOCUSED;
+
+                if (timer != null) {
+                    timer.cancel();
+                    textView.setText("00:00");
+                }
+
+                timer = new CountDownTimer(1800000, 1000) {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        long seconds = millisUntilFinished / 1000;
+                        textView.setText(DateUtils.formatElapsedTime(recycled, seconds));
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        textView.setText("00:00");
+                    }
+                };
+                timer.start();
+
+                break;
+            case R.id.btn_short_break:
+
+                state = CHRONOSTATE.SHORT_BREAKED;
+
+                if (timer != null) {
+                    timer.cancel();
+                    textView.setText("00:00");
+                }
+
+                timer = new CountDownTimer(300000, 1000) {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        long seconds = millisUntilFinished / 1000;
+                        textView.setText(DateUtils.formatElapsedTime(recycled, seconds));
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        textView.setText("00:00");
+                    }
+                };
+                timer.start();
+
+
+                break;
+            case R.id.btn_long_break:
+
+                state = CHRONOSTATE.LONG_BREAKED;
+
+                if (timer != null) {
+                    timer.cancel();
+                    textView.setText("00:00");
+                }
+
+                timer = new CountDownTimer(900000, 1000) {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        long seconds = millisUntilFinished / 1000;
+                        textView.setText(DateUtils.formatElapsedTime(recycled, seconds));
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        textView.setText("00:00");
+                    }
+                };
+                timer.start();
+
+                break;
+            case R.id.btn_stop:
+
+                state = CHRONOSTATE.STOPPED;
+
+                if (timer != null) {
+                    timer.cancel();
+                    textView.setText("00:00");
+                }
+
+                break;
+
+        }
+
+    }
+
+
 }
